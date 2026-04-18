8a6dadd4bab45bbae24fb2c7b159d51476c9b896
Bhushan Kalode <bhushan.kalode@inkind.com>
2026-04-19 01:46:15 +0530

    Add account model with validations and auto account number

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,19 @@
 class Account < ApplicationRecord
   belongs_to :user
+
+  validates :balance, numericality: { greater_than_or_equal_to: 0 }
+  validates :account_number, uniqueness: true
+  validates :ifsc_code, presence: true
+  validates :branch_name, presence: true
+
+  before_create :generate_account_number
+
+  private
+
+  def generate_account_number
+    self.account_number ||= loop do
+      num = "ACC#{SecureRandom.random_number(10**10)}"
+      break num unless Account.exists?(account_number: num)
+    end
+  end
 end

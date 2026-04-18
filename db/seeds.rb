292703a0af604f14c40a09f17b32dd011f5109d5
Bhushan Kalode <bhushan.kalode@inkind.com>
2026-04-19 02:00:10 +0530

    Seed users and accounts

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Alice as a user
+ActiveRecord::Base.transaction do
+  alice = User.create!(name: "Alice", email: "alice@example.com", pin: "1234")
+  Account.create!(user: alice,
+    balance: 1000,
+    ifsc_code: "HDFC0001234",
+    branch_name: "Pune",
+    address: "Pune Main Branch"
+  )
+end
+
+# Bob as a user
+ActiveRecord::Base.transaction do
+  bob = User.create!(name: "Bob", email: "bob@example.com", pin: "4321")
+  Account.create!(
+    user: bob,
+    balance: 500,
+    ifsc_code: "HDFC0001234",
+    branch_name: "Pune",
+    address: "Pune Branch"
+  )
+end

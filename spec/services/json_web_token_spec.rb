57d0c1b76e6ca3ceb207dc98931aa44ce91c066e
Bhushan Kalode <bhushan.kalode@inkind.com>
2026-04-19 12:07:28 +0530

    Added json web token class decode test cases

--- spec/services/json_web_token_spec.rb
+++ spec/services/json_web_token_spec.rb
@@ -10,4 +10,30 @@ RSpec.describe JsonWebToken do
       expect(token).to be_a(String)
     end
   end
+
+  describe '.decode' do
+    it 'returns decoded payload' do
+      token = described_class.encode(payload)
+      decoded_payload = described_class.decode(token)
+
+      expect(decoded_payload[:user_id]).to eq(1)
+    end
+
+    it 'returns nil for invalid token' do
+      expect(described_class.decode('invalid token')).to be_nil
+    end
+
+    it 'returns nil for expired token' do
+      token = described_class.encode(payload, 1.second.ago)
+      expect(described_class.decode(token)).to be_nil
+    end
+
+    it 'returns hash with indifferent access' do
+      token = described_class.encode(payload)
+      decoded = described_class.decode(token)
+
+      expect(decoded['user_id']).to eq(1)
+      expect(decoded[:user_id]).to eq(1)
+    end
+  end
 end
